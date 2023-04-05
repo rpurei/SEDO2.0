ceda02a387bf505f0a432238080a2fd69d56be52
Purey Roman <purey.rp@zdmail.ru>
2023-04-05 16:04:58 +0300

    Замена библиотеки на PrimeNG

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'page', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
