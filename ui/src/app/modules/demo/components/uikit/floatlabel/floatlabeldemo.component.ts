c3a1f3f1c2aeb6075112b7b777ec1938883f591b
Purey Roman <purey.rp@zdmail.ru>
2023-04-07 08:44:08 +0300

    Локализация демо - Название поля

--- ui/src/app/modules/demo/components/uikit/floatlabel/floatlabeldemo.component.ts
+++ ui/src/app/modules/demo/components/uikit/floatlabel/floatlabeldemo.component.ts
@@ -7,42 +7,26 @@ import { CountryService } from "../../../service/country.service";
 export class FloatLabelDemoComponent implements OnInit {
 
     countries: any[] = [];
-
     cities: any[];
-
     filteredCountries: any[] = [];
-
     value1: any;
-
     value2: any;
-
     value3: any;
-
     value4: any;
-
     value5: any;
-
     value6: any;
-
     value7: any;
-
     value8: any;
-
     value9: any;
-
     value10: any;
-
     value11: any;
-
     value12: any;
 
     constructor(private countryService: CountryService) {
         this.cities = [
-            {name: 'New York', code: 'NY'},
-            {name: 'Rome', code: 'RM'},
-            {name: 'London', code: 'LDN'},
-            {name: 'Istanbul', code: 'IST'},
-            {name: 'Paris', code: 'PRS'}
+            {name: 'Белгород', code: 'BLG'},
+            {name: 'Воронеж', code: 'VRN'},
+            {name: 'Курск', code: 'KRS'}
         ];
     }
 
