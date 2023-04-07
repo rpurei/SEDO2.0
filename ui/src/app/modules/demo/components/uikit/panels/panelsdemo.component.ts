008d097ec3978871ed6399a9e41b9483717fc50c
Purey Roman <purey.rp@zdmail.ru>
2023-04-07 14:29:57 +0300

    Добавлена локализация демо - Панель

--- ui/src/app/modules/demo/components/uikit/panels/panelsdemo.component.ts
+++ ui/src/app/modules/demo/components/uikit/panels/panelsdemo.component.ts
@@ -7,26 +7,25 @@ import { MenuItem } from 'primeng/api';
 export class PanelsDemoComponent implements OnInit {
 
     items: MenuItem[] = [];
-
     cardMenu: MenuItem[] = [];
 
     ngOnInit() {
         this.items = [
             { label: 'Angular.io', icon: 'pi pi-external-link', url: 'http://angular.io' },
-            { label: 'Theming', icon: 'pi pi-bookmark', routerLink: ['/theming'] }
+            { label: 'Темы', icon: 'pi pi-bookmark', routerLink: ['/theming'] }
         ];
 
         this.cardMenu = [
             {
-                label: 'Save', icon: 'pi pi-fw pi-check'
+                label: 'Сохранить', icon: 'pi pi-fw pi-check'
             },
             {
-                label: 'Update', icon: 'pi pi-fw pi-refresh'
+                label: 'Обновить', icon: 'pi pi-fw pi-refresh'
             },
             {
-                label: 'Delete', icon: 'pi pi-fw pi-trash'
+                label: 'Удалить', icon: 'pi pi-fw pi-trash'
             },
         ];
     }
-    
+
 }
