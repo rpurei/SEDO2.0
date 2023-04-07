c2004959ec54d3e3c7da3d535b88f17a1b2ccc14
Purey Roman <purey.rp@zdmail.ru>
2023-04-07 08:26:34 +0300

    Локализация демо - Ввод

--- ui/src/app/modules/demo/components/uikit/input/inputdemo.component.ts
+++ ui/src/app/modules/demo/components/uikit/input/inputdemo.component.ts
@@ -8,39 +8,22 @@ import { CountryService } from "../../../service/country.service";
 export class InputDemoComponent implements OnInit {
 
     countries: any[] = [];
-
     filteredCountries: any[] = [];
-
     selectedCountryAdvanced: any[] = [];
-
     valSlider = 50;
-
     valColor = '#424242';
-
     valRadio: string = '';
-
     valCheck: string[] = [];
-
     valCheck2: boolean = false;
-
     valSwitch: boolean = false;
-
     cities: SelectItem[] = [];
-
     selectedList: SelectItem = { value: '' };
-
     selectedDrop: SelectItem = { value: '' };
-
     selectedMulti: any[] = [];
-
     valToggle = false;
-
     paymentOptions: any[] = [];
-
     valSelect1: string = "";
-
     valSelect2: string = "";
-
     valueKnob = 20;
 
     constructor(private countryService: CountryService) { }
@@ -51,17 +34,15 @@ export class InputDemoComponent implements OnInit {
         });
 
         this.cities = [
-            { label: 'New York', value: { id: 1, name: 'New York', code: 'NY' } },
-            { label: 'Rome', value: { id: 2, name: 'Rome', code: 'RM' } },
-            { label: 'London', value: { id: 3, name: 'London', code: 'LDN' } },
-            { label: 'Istanbul', value: { id: 4, name: 'Istanbul', code: 'IST' } },
-            { label: 'Paris', value: { id: 5, name: 'Paris', code: 'PRS' } }
+            { label: 'Белгород', value: { id: 1, name: 'Белгород', code: 'BLG' } },
+            { label: 'Воронеж', value: { id: 2, name: 'Воронеж', code: 'VRN' } },
+            { label: 'Курск', value: { id: 3, name: 'Курск', code: 'KRS' } }
         ];
 
         this.paymentOptions = [
-            { name: 'Option 1', value: 1 },
-            { name: 'Option 2', value: 2 },
-            { name: 'Option 3', value: 3 }
+            { name: 'Опция 1', value: 1 },
+            { name: 'Опция 2', value: 2 },
+            { name: 'Опция 3', value: 3 }
         ];
     }
 
