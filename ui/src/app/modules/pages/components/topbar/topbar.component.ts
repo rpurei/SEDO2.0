bb73e1b9064a2d5ba6f64d4b869072562f281f63
rpurei <rpurey@gmail.com>
2023-04-09 10:41:18 +0300

    Вызов конфигураторы темы из хедера

--- ui/src/app/modules/pages/components/topbar/topbar.component.ts
+++ ui/src/app/modules/pages/components/topbar/topbar.component.ts
@@ -1,19 +1,25 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LayoutService } from "../../../../services/layout.service";
 import { MenuItem } from "primeng/api";
+import { ConfigComponent } from "../../../config/config/config.component";
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.scss']
+  styleUrls: ['./topbar.component.scss'],
+  providers: [ConfigComponent]
 })
 
 export class TopbarComponent {
-    items!: MenuItem[];
+  items!: MenuItem[];
 
-    @ViewChild('menubutton') menuButton!: ElementRef;
-    @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
-    @ViewChild('topbarmenu') menu!: ElementRef;
+  @ViewChild('menubutton') menuButton!: ElementRef;
+  @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
+  @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+  constructor(public layoutService: LayoutService, private configComponent: ConfigComponent) { }
+
+  configClick(): void {
+    this.configComponent.onConfigButtonClick();
+  }
 }
